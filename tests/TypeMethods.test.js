9a7a17bb812607afbdaa5d038a5ca8a6dac7064a
Marco Scoppetta <marco@grakn.ai>
2018-06-12 20:24:09 +0100

    add test for getDataType()

--- tests/TypeMethods.test.js
+++ tests/TypeMethods.test.js
@@ -1,30 +1,32 @@
 const gc = require("../src/GraknSession");
 const DEFAULT_URI = "localhost:48555";
-const DEFAULT_CREDENTIALS = { username: "cassandra", password: "cassandra" };
 const environment = require('./support/GraknTestEnvironment');
 
 const session = new gc(DEFAULT_URI, environment.newKeyspace());
 
 // AttributeType test 
-test.only("Create attribute instance", async (done) => {
+test("Create attribute instance", async (done) => {
     try {
         const tx = await session.open(session.txType.WRITE);
         const attributeType = await tx.putAttributeType("firstname", session.dataType.STRING);
+        expect(await attributeType.getDataType()).toBe('String');
         const attribute = await attributeType.putAttribute('Marco');
         expect(attribute.isAttribute()).toBeTruthy();
         expect(await attribute.getValue()).toBe('Marco');
         expect(await attribute.dataType()).toBe('String');
 
         const boolAttributeType = await tx.putAttributeType("employed", session.dataType.BOOLEAN);
+        expect(await boolAttributeType.getDataType()).toBe('Boolean');
         const boolAttribute = await boolAttributeType.putAttribute(false);
         expect(await boolAttribute.getValue()).toBe(false);
         expect(await boolAttribute.dataType()).toBe('Boolean');
 
 
-        const floatAttributeType = await tx.putAttributeType("length", session.dataType.DOUBLE);
-        const floatAttribute = await floatAttributeType.putAttribute(11.58);
-        expect(await floatAttribute.getValue()).toBe(11.58);
-        expect(await floatAttribute.dataType()).toBe('Double');
+        const doubleAttributeType = await tx.putAttributeType("length", session.dataType.DOUBLE);
+        expect(await doubleAttributeType.getDataType()).toBe('Double');
+        const doubleAttribute = await doubleAttributeType.putAttribute(11.58);
+        expect(await doubleAttribute.getValue()).toBe(11.58);
+        expect(await doubleAttribute.dataType()).toBe('Double');
 
         //TODO add check expect.toThrow
         done();
